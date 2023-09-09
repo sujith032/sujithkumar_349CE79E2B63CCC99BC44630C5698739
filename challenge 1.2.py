1ea28a859b7b600684ed2dd2ee23cdbc502be4ef
sujith032 <144546121+sujith032@users.noreply.github.com>
2023-09-09 21:52:26 +0530

    Add files via upload

---
+++ challenge 1.2.py
@@ -0,0 +1,17 @@
+# Leap year
+"""
+Year % 4 == 0 & 
+Year % 100 != 0 / 
+Year % 400 == 0 
+
+"""
+def is LeapYear(year):
+  if (year % 4== 0 and year % 100 !=0) or year % 400 ==0 :
+    return True 
+  else:
+      return False
+year = 2012
+if is LeapYear(year):
+  print("{} is not a leap year.". format(year))
+else:
+    print("{} is not a leapyear.".format(year))
